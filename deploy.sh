2d4453a39193e7b9d35d251eb1389a9af32cfb03
Ilias <mail@ilias.xyz>
2017-02-17 10:36:44 +0100

    force add js/css

--- deploy.sh
+++ deploy.sh
@@ -42,18 +42,9 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 ls -l
 
-git status
-git diff
-
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code > /dev/null` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
+git add --force sweeper.js sweeper.css
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
